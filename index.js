aff4c4aeba5b7950436746abc7ee14c9dcda4b31
Don Chisolm <don.chisolm14@gmail.com>
2021-09-06 01:43:06 -0400

    questions created for RM creation

--- index.js
+++ index.js
@@ -1,7 +1,68 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username.',
+    },
+   
+    {
+        type: 'input',
+        name: 'repository',
+        message: 'Enter the name of your repository on GitHub.',
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project.',
+    },
+    
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project.',
+    },
+    
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Explain how user would install (if necessary) for Installation Section.',
+        
+    },
+    
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter your project instructions and examples of it in use for Usage Section.',
+    },
+    
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license for your project.',
+        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
+        
+    },
+
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Explain how users can contribute to your project (if necessary).',
+       
+    },
+   
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide tests for project, and explain how to test (if necessary).',
+    },
+]; 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
