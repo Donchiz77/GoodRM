82d5b87094924d9b18a01fa391eb79695d99d38c
Don Chisolm <don.chisolm14@gmail.com>
2021-09-06 09:25:46 -0400

    index fixed for prompt functionality and RM gen updated to render prompt answers

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
-const generateMarkdown = require("./utils/generateMarkdown");
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -46,7 +46,7 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'Choose your license for your project.',
-        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
+        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'MIT', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
         
     },
 
@@ -66,22 +66,18 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('Your markdown file has been created.')
-    });
-}
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+};
 
 // TODO: Create a function to initialize app
 function init() {
-    const userResponses = await inquirer.prompt(questions);
-        console.log("Your responses: ", userResponses);
-        console.log("Your responses have been logged. Calling to GitHub...");
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        // console.log("Your responses: ", userResponses);
+        console.log("Generating README...");
         
-        await writeFileAsync('ExampleREADME.md', markdown); 
-}
+         writeToFile('ExampleREADME.md', generateMarkdown({...inquirerResponses})); 
+});
+};
 
 // Function call to initialize app
 init();
